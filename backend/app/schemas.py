aae0d3f63b41d22c95c7158f8d0a529fa08a4b95
Adama Ba <adama.ba@edu.univ-paris13.fr>
2026-03-19 18:37:45 +0100

    shémas Pydantic pour validation et sérialisation des données

---
+++ backend/app/schemas.py
@@ -0,0 +1,76 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class CategorieBase(BaseModel):
+    name: str
+
+class CategorieResponse(CategorieBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+class IndicateurBase(BaseModel):
+    name: str
+    unit: str |None = None
+    category_id: int
+    description: str |None = None
+
+class IndicateurResponse(IndicateurBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+class SourceAPIBase(BaseModel):
+    name: str
+    url_base: str
+    category_id: int
+
+class SourceAPIResponse(SourceAPIBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+class TendanceBase(BaseModel):
+    valeur: float
+    pays: str | None = None
+    source_id: int
+    indicateur_id: int
+class TendanceResponse(TendanceBase):
+    id: int
+    date_heure: datetime
+    class Config:
+        from_attributes = True
+
+class HistoriqueBase(BaseModel):
+    valeur: float
+    pays: str | None = None
+    indicateur_id: int
+
+class HistoriqueResponse(HistoriqueBase):
+    id: int
+    date_heure: datetime
+    class Config:
+        from_attributes = True
+class UserBase(BaseModel):
+    name: str
+    first_name: str
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    first_name: str
+    email: str
+    date_inscription: datetime
+    class Config:
+        from_attributes = True
+class CommentaireBase(BaseModel):
+    text: str
+    tendance_id: int
+    id_user: int
+class CommentaireResponse(CommentaireBase):
+    id: int
+    date_heure: datetime
+    class Config:
+        from_attributes = True
