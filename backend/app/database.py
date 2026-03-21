f2de4b2e7f4ef6f9d79ce441eb1d752746fb13e4
Nina987-dev <chcelina22@gmail.com>
2026-03-21 08:11:47 +0100

    Update database.py
    Mise en place de la couche d'accès aux données (DAL) et gestion des sessions.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,27 +1,22 @@
-
+import psycopg2
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Charge les variables du fichier .env
-load_dotenv()
+def get_conn():
+    return psycopg2.connect(
+        user='postgres',
+        password='root', 
+        host='localhost',
+        port='5432',
+        database='tendance_db'
+    )
 
-# Récupère l'URL de connexion depuis le.env
-DATABASE_URL = os.getenv("DATABASE_URL")
+# j'utilise "sqlite://" pour que SQLAlchemy ne cherche PAS le plugin postgresql
+engine = create_engine("sqlite://", creator=get_conn)
 
-# Moteur SQLAlchemy — c'est lui qui parle à PostgreSQL
-engine = create_engine(DATABASE_URL)
-
-# SessionLocal — chaque requête aura sa propre session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base — classe mère que toutes les classes de models.py héritent
 Base = declarative_base()
 
-
-# Fonction utilitaire — fournit une session à chaque endpoint
 def get_db():
     db = SessionLocal()
     try:
